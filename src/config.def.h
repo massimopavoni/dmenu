f75a0ec7a078eab2982ffb759fe664572ab6a0e1
Massimo Pavoni <maspavoni@gmail.com>
2024-03-23 01:32:45 +0100

    Vertical

--- src/config.def.h
+++ src/config.def.h
@@ -9,7 +9,7 @@ static int fuzzy = 1;                       /* -F option; if 0, dmenu doesn't us
 static const char *fonts[] = {
 	"monospace:size=10"
 };
-static const char *prompt      = NULL;      /* -p option; prompt to the left of input field */
+static const char *prompt = NULL;      /* -p option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
 	[SchemeNorm] = { "#bbbbbb", "#222222" },
@@ -17,7 +17,7 @@ static const char *colors[SchemeLast][2] = {
 	[SchemeOut] = { "#000000", "#00ffff" },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
-static unsigned int lines      = 0;
+static unsigned int lines = 16;
 
 /*
  * Characters not considered part of a word while deleting words
